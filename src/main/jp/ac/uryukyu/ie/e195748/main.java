459a23591bf4e73044629cd012bb506b6fdf6500
Hikaru Tokuzato <e195748@ie.u-ryukyu.ac.jp>
2019-11-27 12:37:21 +0900

    step2 commit

--- src/main/jp/ac/uryukyu/ie/e195748/main.java
+++ src/main/jp/ac/uryukyu/ie/e195748/main.java
@@ -2,7 +2,12 @@ package jp.ac.uryukyu.ie.e195748;
 
 public class main {
     public static void main(String[] args){
-        String str = null;
-        System.out.print(str.length());
+        try {
+            String str = null;
+            System.out.print(str.length());
+        } catch (NullPointerException n){
+            System.out.println("ぬるぽが発生しました");
+            System.out.println(n.getMessage());
+        }
     }
 }
